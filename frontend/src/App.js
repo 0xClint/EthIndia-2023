5164c861ac9a18a6ce31f1735fedb9b1615b2cd5
0xClint <omkarbdarde@gmail.com>
2023-12-09 11:48:31 +0530

    keyboard controls added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei';
 import { Suspense } from "react";
-import { Tree } from './GameComponents';
 import { Physics } from '@react-three/rapier';
 import { SocketManager } from './GameComponents/SocketManager';
 import { Experience } from './GameComponents/Experience';
